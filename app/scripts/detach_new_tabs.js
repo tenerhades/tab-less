1173f0b562ef69992675aaf557268c8693955b57
Tener Hades <tener.hades@gmail.com>
2017-01-23 10:38:44 -0600

    added icons back (what is grunt?) and made all new tabs type: 'popup'

--- app/scripts/detach_new_tabs.js
+++ app/scripts/detach_new_tabs.js
@@ -7,7 +7,7 @@ var detachTab = function(tab) {
   }
 
   chrome.windows.get(tab.windowId, function(oldWindow) {
-    chrome.windows.create({'tabId': tab.id, 'incognito': oldWindow.incognito}, function(newWindow) {
+    chrome.windows.create({'tabId': tab.id, 'incognito': oldWindow.incognito, type: 'popup'}, function(newWindow) {
       chrome.windows.update(newWindow.id, {'state': oldWindow.state});
     });
   });
